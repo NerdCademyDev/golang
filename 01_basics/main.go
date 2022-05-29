4f77ff69de28698da1a0b2de38b7390e59759bb3
Bello mahmud <75342173+mahmudsudo@users.noreply.github.com>
2022-05-29 22:11:30 +0100

    updated imports
    added different types of imports

--- 01_basics/main.go
+++ 01_basics/main.go
@@ -1,6 +1,7 @@
 package main
 
 import "fmt"
+/
 
 // this is a single line comment
 /*
@@ -8,6 +9,16 @@ import "fmt"
 	is a 
 	multiline 
 	comment
+*/
+/*
+imports from external libraries can be local(from your workspace) or public(github or any other repositories)
+e.g:
+public :
+import "github.com/NerdCademy/golang/01_basics"
+local : 
+import "golang/01_basics"
+
+NB: local imports will only work after initialising the src module by using ```git mod init "midule_name"``` in this example "git mod init golang"
 */
 func main() {
 	fmt.Println("Hello, World!")
